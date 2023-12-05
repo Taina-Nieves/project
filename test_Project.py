4ac2d6f5d30e8651e7b164e6f202d5d333dd32a8
Taina-Nieves <lilninanieves@gmail.com>
2023-12-05 16:57:13 -0500

    First test for Category Generator

--- test_Project.py
+++ test_Project.py
@@ -1,8 +1,29 @@
 import unittest
+from Project import CategoryGenerator, Hangman, Challange
+
+"""
+You should have one TestCase class for each of your classes that you test.
+Your tests should exercise a reasonable set of expected behaviors for your classes.
+"""
+class TestCategoryGenerator(unittest.TestCase):
+    def test_getcat(self):
+            generator = CategoryGenerator()
+            word = generator.getCatWord()
+            self.assertIn(word, ["newyork", "losangeles", "chicago", "miami", "houston", "dallas", "albany",
+                                "orlando", "boston", "colorado","stamford", "newark", "tampa", "honolulu",
+                                "cambridge", "buffalo","northamerica", "southamerica", "europe", "asia",
+                                "africa", "australia", "antartica","usa", "canada", "mexico", "brazil", 
+                                "guatemala""argentina", "ecuador", "bolivia", "colombia", "peru"
+                                "panama", "venezuela", "honduras", "belize", "barbados"])
+
+#class TestHangman(unittest.TestCase):
+     
+
+#class TestChallange(unittest.TestCase):
 
 
 
 
 
-
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
